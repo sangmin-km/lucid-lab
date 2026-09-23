f939f6f34d73f1104ea86129c65619576156ae02
김상민 <sangmin@gimsangmin-ui-MacBookAir.local>
2026-09-23 15:14:20 +0900

    Update publications, PRISM, contacts, and footer

--- forms/contact.php
+++ forms/contact.php
@@ -6,8 +6,8 @@
   * For more info and help: https://bootstrapmade.com/php-email-form/
   */
 
-  // Replace hyunae0608@gmail.com with your real receiving email address
-  $receiving_email_address = 'hyunae0608@gmail.com';
+  // Replace hyunae0608.jung@gmail.com with your real receiving email address
+  $receiving_email_address = 'hyunae0608.jung@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
